08137e20e4bf17a13672b9e5def7682454688874
shfeekalbhure <shfeek01@gmail.com>
2026-07-21 21:35:40 +0300

    تسجيل إعدادات النظام متعددة النطاق

--- AlTayerERP.Infrastructure/Data/AppDbContext.cs
+++ AlTayerERP.Infrastructure/Data/AppDbContext.cs
@@ -56,6 +56,9 @@ namespace AlTayerERP.Infrastructure.Data
 
         #region 3. إعدادات الترقيم والرقابة والاعتمادات
 
+        // جدول إعدادات عامة قابلة للتخصيص على مستوى النظام/الشركة/الفرع/السنة.
+        public DbSet<SystemSetting> System_Settings { get; set; } = null!;
+
         // جدول إعدادات ترميز وترقيم المستندات (مثل السندات والفواتير) لكل فرع
         public DbSet<NumberingSetting> Numbering_Settings { get; set; } = null!;
 
