2749989f24916397ba78f642a92e00c6c07897c5
shfeekalbhure <shfeek01@gmail.com>
2026-07-21 23:03:39 +0300

    Restore classic form appearance

--- AlTayerERP.Desktop/FrmCurrencies.cs
+++ AlTayerERP.Desktop/FrmCurrencies.cs
@@ -34,9 +34,7 @@ namespace AlTayerERP.Desktop
             InitializeComponent();
 
             // تطبيق الثيم العربي الموحد والاختصارات على الشاشة القديمة.
-            ArabicErpFormStyle.Apply(this);
-
-            RegisterEvents();
+RegisterEvents();
         }
 
         #endregion
