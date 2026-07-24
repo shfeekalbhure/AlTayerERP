2fdbb1e33d7c6f65bb17ce8b8149f38df257f4d2
shfeekalbhure <shfeek01@gmail.com>
2026-07-24 20:31:10 +0300

    تسجيل شاشة طلب الصرف في كتالوج الصلاحيات

--- AlTayerERP.API/Services/SystemScreenCatalogSeeder.cs
+++ AlTayerERP.API/Services/SystemScreenCatalogSeeder.cs
@@ -47,6 +47,7 @@ namespace AlTayerERP.API.Services
                 new ScreenSeed("Parties", "الأطراف المالية", "الحسابات", 210),
                 new ScreenSeed("ReceiptVoucher", "سند القبض", "الحسابات", 220),
                 new ScreenSeed("PaymentVoucher", "سند الصرف", "الحسابات", 230),
+                new ScreenSeed("PaymentRequest", "طلب الصرف", "الحسابات", 235),
                 new ScreenSeed("JournalVoucher", "القيد اليومي", "الحسابات", 240),
                 new ScreenSeed("DocumentSearch", "البحث عن المستندات", "الحسابات", 250),
                 new ScreenSeed("ApprovalRequests", "طلبات الاعتماد", "الحسابات", 260),
