77983e757abdccec0e05fe0cb81af2c60aefa33e
shfeekalbhure <shfeek01@gmail.com>
2026-07-26 20:31:27 +0300

    تهيئة خدمات تطبيق الجوال والاتصال بالـAPI

--- AlTayerERP.Mobile.Office/MauiProgram.cs
+++ AlTayerERP.Mobile.Office/MauiProgram.cs
@@ -1,25 +1,34 @@
-﻿using Microsoft.Extensions.Logging;
+using AlTayerERP.Mobile.Office.Services;
+using Microsoft.Extensions.Logging;
 
-namespace AlTayerERP.Mobile.Office
+namespace AlTayerERP.Mobile.Office;
+
+public static class MauiProgram
 {
-    public static class MauiProgram
+    public static MauiApp CreateMauiApp()
     {
-        public static MauiApp CreateMauiApp()
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+        builder.Services.AddSingleton(new HttpClient
         {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+            BaseAddress = new Uri("https://10.0.2.2:5001/"),
+            Timeout = TimeSpan.FromSeconds(30)
+        });
+        builder.Services.AddSingleton<SessionStorageService>();
+        builder.Services.AddSingleton<AuthenticationService>();
+        builder.Services.AddTransient<MainPage>();
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
 
-            return builder.Build();
-        }
+        return builder.Build();
     }
 }
