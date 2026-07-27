cae623918bdff8eaa730a7c156aaea7ed5fba972
shfeekalbhure <shfeek01@gmail.com>
2026-07-27 20:03:00 +0300

    تحديث عنوان API لتطبيق الجوال

--- AlTayerERP.Mobile.Office/MauiProgram.cs
+++ AlTayerERP.Mobile.Office/MauiProgram.cs
@@ -19,9 +19,10 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
 
+        // عنوان جهاز الخادم داخل الشبكة المحلية. لا تستخدم localhost من الجوال.
         builder.Services.AddSingleton(new HttpClient
         {
-            BaseAddress = new Uri("http://172.16.4.250:5021/"),
+            BaseAddress = new Uri("http://172.16.4.192:5021/"),
             Timeout = TimeSpan.FromSeconds(30)
         });
         builder.Services.AddSingleton<SessionStorageService>();
